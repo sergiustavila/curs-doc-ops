3201dd62c3dadf0eb5a01256439bdf0c93b4df8e
sergiustavila <sergiu.stavila@gmail.com>
2023-10-17 19:37:49 +0300

    Set another theme

--- source/conf.py
+++ source/conf.py
@@ -13,6 +13,8 @@ author = 'Sergiu'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+import sphinx_rtd_theme
+
 extensions = [
     "sphinx_rtd_theme",
     "sphinx.ext.duration",
@@ -31,5 +33,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
